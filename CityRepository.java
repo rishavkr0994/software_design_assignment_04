3e3a7bb375d63220b4908e4f6734c76752a5693a
sasankagali8444 <94205649+sasankagali8444@users.noreply.github.com>
2021-11-15 21:59:21 -0700

    Add files via upload

---
+++ CityRepository.java
@@ -0,0 +1,29 @@
+public class CityRepository implements Container {
+   ArrayList<City> cityList = new ArrayList<>;
+
+   @Override
+   public Iterator getIterator() {
+      return new CityIterator();
+   }
+
+   private class CityIterator implements Iterator {
+
+      int index;
+
+      public boolean hasNext() {
+
+         if(index < cityList.size()){
+            return true;
+         }
+         return false;
+      }
+
+      public Object next() {
+
+         if(this.hasNext()){
+            return cityList[index++];
+         }
+         return null;
+      }
+   }
+}
